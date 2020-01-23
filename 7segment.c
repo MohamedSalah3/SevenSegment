7bac55c336670560a932985384fe63d6b64fd108
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-01-23 22:00:35 +0200

    .

--- 7segment.c
+++ 7segment.c
@@ -19,8 +19,10 @@ uint8_t Prescaler_Value=0;
 
 int main(void)
 {
+	uint8_t flag=0,counter=0,counter10=0,Loop_counter=0;
+	uint8_t *ptru8_counter=&counter;
+	uint8_t *ptru8_counter10=&counter10;
 timer0Init(T0_NORMAL_MODE,T0_OC0_DIS,T0_PRESCALER_8 ,0,0,T0_INTERRUPT_NORMAL);
-uint8_t flag=0,counter=0,counter10=0,Loop_counter=0;
 sevenSegInit(SEG_0);
 sevenSegInit(SEG_1);
 //gpioPortDirection(GPIOD,HIGH);
@@ -32,21 +34,9 @@ sevenSegInit(SEG_1);
 for (Loop_counter=0;Loop_counter<10;Loop_counter++)
 {
 	flag=0;
-	sevenSegDisable(SEG_0);
-	sevenSegEnable(SEG_1);
-	sevenSegWrite(SEG_1,counter);
-	timer0Start();
-	timer0DelayMs(50);
-	sevenSegDisable(SEG_1);
-	sevenSegEnable(SEG_0);
-	sevenSegWrite(SEG_0,counter10);
-	/*softwareDelayMs(50);*/
-	timer0Start();
-	timer0DelayMs(50);
-
 /*softwareDelayMs(50);*/
 
-if (counter >= 10)
+if (counter >= 9 || counter10 >=9)
 {
 	flag=1;
 	counter=0;
@@ -58,12 +48,24 @@ if (counter >= 10)
 	{
 	  	counter10++;
 	}
-	timer0Start();
-	timer0DelayMs(500);
-
+timer0Start();
+timer0DelayMs(500);
+for (Loop_counter=0;Loop_counter<10;Loop_counter++)
+{
+sevenSegDisable(SEG_0);
+sevenSegEnable(SEG_1);
+sevenSegWrite(SEG_1,*ptru8_counter);
+timer0Start();
+timer0DelayMs(10);
+sevenSegDisable(SEG_1);
+sevenSegEnable(SEG_0);
+sevenSegWrite(SEG_0,*ptru8_counter10);
+/*softwareDelayMs(50);*/
 }
-
-        //TODO:: Please write your application code
+timer0Start();
+timer0DelayMs(500);//TODO:: Please write your application code
+}
+//TODO:: Please write your application code
     }
 }
 void __vector_11(void) __attribute__((signal,__INTR_ATTRS));
